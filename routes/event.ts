7ff1b74df9eb846436f31114b90782c01238ecbc
sambyviiva <147328245+sambyviiva@users.noreply.github.com>
2023-11-22 21:23:49 +0000

    Update event.ts

--- routes/event.ts
+++ routes/event.ts
@@ -5,7 +5,7 @@ import { Event } from "../models/event.model";
 export const eventRoute = Router();
 
 eventRoute.get("/event", (req: Request, res: Response) => {
-  Event.find({}, { name: true, _id: 0, startDate: true, endDate: true })
+  Event.find({}, { name: true, _id: true, startDate: true, endDate: true })
     .then((events) => res.json(events))
     .catch((err) => res.status(400).json("Error: " + err));
 });
